82a60a8a79bac419f579b9944f88f16cb30da61f
Inzimamul Ansari <inzamamansari360@gmail.com>
2024-05-30 04:12:32 +0530

    Initial Commit

---
+++ NSDLDemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NSDLDemoApp
+//
+//  Created by Inzimamul Ansari on 30/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NSDLDemoApp/NSDLDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NSDLDemoAppApp.swift
+//  NSDLDemoApp
+//
+//  Created by Inzimamul Ansari on 30/05/24.
+//
+
+import SwiftUI
+
+@main
+struct NSDLDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
