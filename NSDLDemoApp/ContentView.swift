5a213a1f309b839ab44a44d73cbf3ca080747ba9
Inzimamul Ansari <inzamamansari360@gmail.com>
2024-05-30 04:18:28 +0530

    Initial commit - NSDL Demo App

--- NSDLDemoApp/ContentView.swift
+++ NSDLDemoApp/ContentView.swift
@@ -6,19 +6,36 @@
 //
 
 import SwiftUI
+import NSDLSwiftPackage
 
 struct ContentView: View {
+    @State private var email: String = "No email fetched yet"
+    @State private var showingUserListView = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Button(action: { showingUserListView = true }) {
+                Text("Call Package")
+            }
+            .frame(width: 150, height: 50)
+            .border(Color.black)
+            .padding(.bottom, 100)
+            Text(email)
+                .frame(width: 200, height: 100)
+                .border(Color.black)
+                .padding(.bottom, 100)
         }
-        .padding()
+        .fullScreenCover(isPresented: $showingUserListView) {
+            EmailListView { selectedEmail in
+                self.email = selectedEmail
+                self.showingUserListView = false
+            }
+        }
     }
 }
-
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
